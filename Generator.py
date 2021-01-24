303d43cd74df839e330d59cb72f776818463e5cb
hossein1387 <m.h.askari.hemmat@gmail.com>
2021-01-24 10:12:57 -0500

    adding features to the generator

--- Generator.py
+++ Generator.py
@@ -1,13 +1,13 @@
+from texttable import Texttable
 
 class Generator():
     """docstring for Generator"""
-    def __init__(self, model):
+    def __init__(self, model, prec, input_shape):
         super(Generator, self).__init__()
         # expecting to receive a OnnxModel parsed object
         self.model = model
-        self.iShape = model[]
-        self.fShape = model[]
-        self.stride = model[]
+        self.prec = prec
+        self.input_shape = input_shape
 
     # iprec: Input data precision
     # wprec: Weight precision
@@ -24,19 +24,23 @@ class Generator():
     # Pt   : Zero-padding in on the top in the height dimension
     # Pb   : Zero-padding in on the bottom in the height dimension
 
-    def get_mvu_param(self, prec):
+    def get_mvu_param(self, prec, iShape, fShape, stride):
         iprec,wprec,oprec = prec
-        iW,iH,iC = iShape
-        fW,fH,fC = fShape
+        iC, iH, iW = iShape
+        fC, fH, fW = fShape
         sW = stride 
-        ilength[0] = iC*Fw-1
+        ilength = [0,0,0,0]
+        ijump = [0,0,0,0]
+        wlength = [0,0,0,0]
+        wjump = [0,0,0,0]
+        ilength[0] = iC*fW-1
         ilength[1] = fH-1
         ilength[2] = iprec*wprec*fC-1
         ilength[3] = 0
         ijump[0]   = iprec*(iC*(iW-fW) + 1)
-        ijump[1]   = -iprec*(C*(fH-1)*iW + fW*iC - 1)
-        ijump[2]   = -iprec*(C*(fH-1)*iW + (fW-sW-1)*iC + 1)
-        ijump[3]   = ijump2
+        ijump[1]   = -iprec*(iC*(fH-1)*iW + fW*iC - 1)
+        ijump[2]   = -iprec*(iC*(fH-1)*iW + (fW-sW-1)*iC + 1)
+        ijump[3]   = ijump[2]
         wlength[0] = iC*fW*fH-1
         wlength[1] = iprec*wprec-1
         wlength[2] = fC-1
@@ -44,9 +48,35 @@ class Generator():
         wjump[0] = -wprec*(iC*fW*fH-1)
         wjump[1] = wprec
         wjump[2] = -wprec*(iC*fW*fH*fC-1)
-        wjump[3] = wjump2
-        countdown = (iC * fW) * (fH) * (iprec * wprec) * (fC) * ((W-fW+1)/sW)
+        wjump[3] = wjump[2]
+        countdown = (iC * fW) * (fH) * (iprec * wprec) * (fC) * ((iW-fW+1)/sW)
         return [ilength, ijump, wlength, wjump, countdown]
+
+    def infer_activation_shape(self, input, kernel, padding, stride):
+        iC, iH, iW = input
+        fC, fH, fW = kernel
+        oH=int((iH-fH+2*padding)/stride)+1
+        oW=int((iW-fW+2*padding)/stride)+1
+        oC=fC
+        return [oC, oH, oW]
+
+    def generate_mvu_configs(self):
+        t = Texttable(max_width=120)
+        t.add_row(['ilength', 'ijump', 'wlength', 'wjump', 'countdown'])
+        input_shape = self.input_shape
+        for layer in self.model.parsed_model:
+            import ipdb as pdb; pdb.set_trace()
+            iShape = input_shape
+            fShape = [layer['out_channels'], layer['kernel_size'][0], layer['kernel_size'][1]]
+            stride = layer['stride'][0]
+            padding = layer['padding'][0]
+            prec = self.prec
+            # print("{} * {}".format(iShape, fShape))
+            ilength, ijump, wlength, wjump, countdown = self.get_mvu_param(prec, iShape, fShape, stride)
+            t.add_row([ilength, ijump, wlength, wjump, countdown])
+            input_shape = self.infer_activation_shape(input_shape, fShape, padding, stride)
+        print("\nGenerated MVU configuration:")
+        print(t.draw())
 
     def print_mvU_param(self):
         pass
--- sample_mvu_code_generator.py
+++ sample_mvu_code_generator.py
@@ -1,15 +1,23 @@
 import argparse
 from OnnxParser import OnnxParser
+from Generator import Generator
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-x', '--onnx_model', help='input onnx model', required=True)
-    parser.add_argument('--aprec', help='Activation precision', required=False, default=8)
-    parser.add_argument('--wprec', help='Weight precision', required=False, default=8)
+    parser.add_argument('--aprec', help='Activation precision', required=False, default=2, type=int)
+    parser.add_argument('--wprec', help='Weight precision', required=False, default=2, type=int)
+    parser.add_argument('--oprec', help='Output precision', required=False, default=2, type=int)
+    parser.add_argument('--oprec', help='Output precision', required=False, default=2, type=int)
     args = parser.parse_args()
     return vars(args)
 
 if __name__ == '__main__':
     args = parse_args()
-    parsed_model = OnnxParser(args['onnx_model'])
-    parsed_model.print_onnx_model()
+    model = OnnxParser(args['onnx_model'])
+    model.print_onnx_model()
+    precision = [args['aprec'], args['wprec'], args['oprec']]
+    input_shape = [3,32,32]
+    generator = Generator(model, precision, input_shape)
+    generator.generate_mvu_configs()
+
